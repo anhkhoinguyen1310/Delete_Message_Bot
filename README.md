ac966cab8ac3f02c5aff930099767265309238a0
khoinguyen111 <kan140@scarletmail.rutgers.edu>
2025-04-07 15:39:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Delete_Message_Bot
+- This bot will delete the message from different bot in the same group
---
+++ main.py
@@ -0,0 +1,33 @@
+from telethon import TelegramClient, events
+import os
+
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+session_name = "railway_session"
+
+client = TelegramClient(session_name, api_id, api_hash)
+
+TARGET_KEYWORDS = [
+    "🔥 @Chatkeeperbot - number one group management service in Telegram",
+    "Welcome to Catton Community"
+]
+
+@client.on(events.NewMessage(incoming=True))
+async def handler(event):
+    sender = await event.get_sender()
+    if not sender.bot:
+        return
+
+    text = event.raw_text
+    for keyword in TARGET_KEYWORDS:
+        if keyword in text:
+            try:
+                await event.delete()
+                print("Deleted message:", text)
+            except Exception as e:
+                print("Error deleting:", e)
+            break
+
+print(">> Userbot started.")
+client.start()
+client.run_until_disconnected()
